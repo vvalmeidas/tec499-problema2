e1c88f5d2d44ae6f61d5b717459ab86597573a39
Valmir Vinicius de Almeida <vvalmeida96@gmail.com>
2018-07-01 13:14:52 -0300

    Update uart_rx.v

--- verilog/uart/uart_rx.v
+++ verilog/uart/uart_rx.v
@@ -1,18 +1,24 @@
-module uart_rx(rx, rst, clk, data, ready);
+module uart_rx(rx, rst, clk, data, ready, ready_word, number_received_words);
 	
 	input rx;
 	input clk;
 	input rst;
 	
-	output reg [7:0] data;
+	output reg [31:0] data;
 	output reg ready;
+	output reg ready_word;
+	output reg [10:0] number_received_words;
+	
 	
 	reg [3:0] counter;
+	reg [1:0] counter_received_bytes = 2'd0;
+	reg [10:0] counter_received_words = 11'd0;
 	reg [7:0] dataA;
 	
 	reg [1:0] state;
 	
-	localparam idle= 2'b00, receving = 2'b01, done = 2'b11; 
+	localparam idle= 2'b00, receving = 2'b01, store=2'b10, done = 2'b11; 
+	localparam first_address = 14'h1;
 	
 	always @ (posedge clk) begin
 			if (rst) begin
@@ -32,17 +38,41 @@ module uart_rx(rx, rst, clk, data, ready);
 					end
 					receving:begin
 						if (counter == 4'd8) begin
-							state <= done;
+							state <= store;
 						end else begin
 							dataA[counter] <= rx;
 							counter <= counter + 1'd1;
 							state <= receving;
 						end
 					end
+					
+					store:begin
+						if(counter_received_bytes == 2'd0) begin
+							data[7:0] <= dataA;
+							counter_received_bytes <= counter_received_bytes + 11'd1;
+							ready_word <= 1'b0;
+						end else if(counter_received_bytes == 2'd1) begin
+							data[15:8] <= dataA;
+							counter_received_bytes <= counter_received_bytes + 11'd1;
+							ready_word <= 1'b0;
+						end else if(counter_received_bytes == 2'd2) begin
+							data[23:16] <= dataA;
+							counter_received_bytes <= counter_received_bytes + 11'd1;
+							ready_word <= 1'b0;
+						end else if(counter_received_bytes == 2'd3) begin
+							data[31:24] <= dataA;
+							data <= {data_1, data_2};
+							number_received_words <= number_received_words + 11'd1;
+							counter_received_bytes <= 11'd0;
+							ready_word <= 1'b1;
+						end
+						
+						state <= done;
+					end
+					
 					done:begin
 						ready <= 1'b1;
 						state <= idle;
-						data <= dataA;
 					end
 				endcase
 			end
